05ac090137eede53e490cda5410e4d089bfc9c2d
KevinF <browis7306@gmail.com>
2026-01-04 13:54:07 -0600

    Commit Controlador usuario

--- src/controlador/ctrlUsuario.java
+++ src/controlador/ctrlUsuario.java
@@ -5,10 +5,36 @@
  */
 package controlador;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import modelo.Usuario;
+
 /**
  *
  * @author kevin
  */
 public class ctrlUsuario {
-    
+
+    //metodo para iniciar sesion
+    public boolean LoginUser(Usuario objeto) {
+        boolean respuesta = false;
+        Connection cn = conexion.Conexion.conectar();
+        String sql = "select nombre, pssword from tb_Usuario where nombre= '" + objeto.getNombre() + "' and pssword = '" + objeto.getPsswrd()+"";
+        Statement st;
+        try {
+            st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                respuesta = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al iniciar sesion");
+            JOptionPane.showMessageDialog(null, "Error al iniciar sesion");
+        }
+        return respuesta;
+    }
+
 }
