a872723c8b5947e7762a3dba4bdfbaea20dcbfb9
KevinF <browis7306@gmail.com>
2026-01-04 13:13:18 -0600

    Commit Conexion

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -5,10 +5,24 @@
  */
 package conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author kevin
  */
 public class Conexion {
-    
+
+    public static Connection conectar() {
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/SistemaVentas", "root", "Spooky07_07.");
+            return cn;
+
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión con la base de datos " + e);
+        }
+        return null;
+    }
 }
